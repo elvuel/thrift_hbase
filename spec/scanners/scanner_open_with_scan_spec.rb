584d11acd17fe51b2cdcba1e1d8593ef87f182f1
elvuel <elvuel@gmail.com>
2013-03-31 15:43:42 +0800

    add RowFilter spec for scanner

--- spec/scanners/scanner_open_with_scan_spec.rb
+++ spec/scanners/scanner_open_with_scan_spec.rb
@@ -22,10 +22,14 @@ describe ThriftHbase::Scanner do
 
       10.times do |i|
         row = ThriftHbase::Row.new(table: @table)
-        row.id = "spec#{i}"
+        if i < 5
+          row.id = "spec#{i}"
+        else
+          row.id = "other#{i}"
+        end
         row.columns = {
           'colf1:name' => ThriftHbase::Cell.new(value: "th-gem#{i}"),
-          'colf1:kind' => ThriftHbase::Cell.new(value: "spec"),
+          'colf1:kind' => ThriftHbase::Cell.new(value: "测试"),
           'colf1:kind_id' => ThriftHbase::Cell.new(value: 999),
           'colf1:count' => ThriftHbase::Cell.new(value: i),
           'colf1:created_at' => ThriftHbase::Cell.new(value: Time.now.utc.to_s)
@@ -38,8 +42,95 @@ describe ThriftHbase::Scanner do
       @connection.close
     }
 
-    it 'scanning' do
+    it 'filters' do
 
-    end # scanning
+      # KeyOnlyFilter ()
+      # FirstKeyOnlyFilter ()
+      # PrefixFilter ('<prefix>')
+      # ColumnPrefixFilter ('<col_f1>')
+      # MultipleColumnPrefixFilter ('[col_f1, col_f2]')
+      # ColumnCountGetFilter (<count>)
+      # PageFilter ('<page_size>')
+      # ColumnPaginationFilter (<limit>, <offset>)
+      # InclusiveStopFilter (‘<stop_row_key>’)
+      # TimeStampsFilter (<timestamp>, <timestamp>, ... ,<timestamp>)
+
+      # RowFilter (<compareOp>, ‘<row_comparator>’)
+      # QualifierFilter (<compareOp>,‘<qualifier_comparator>’)
+
+    end # filters
+
+    describe '#RowFilter' do
+      it 'BinaryComparator binary' do
+        t_scan = Apache::Hadoop::Hbase::Thrift::TScan.new
+
+        scanner = ThriftHbase::Scanner.new(@table, [], scanner_to_invoke: :with_scan)
+        t_scan.filterString = "RowFilter (=, 'binary:other')"
+        #t_scan.filterString = "RowFilter (=, 'binaryprefix:other')"
+        scanner.options = {
+          t_scan: t_scan
+        }
+        scanner.open
+        results = scanner.fetch_rows
+        results.size.must_equal 0
+        scanner.close
+      end # BinaryComparator binary
+
+      it 'BinaryPrefixComparator binaryprefix' do
+        t_scan = Apache::Hadoop::Hbase::Thrift::TScan.new
+
+        scanner = ThriftHbase::Scanner.new(@table, [], scanner_to_invoke: :with_scan)
+        t_scan.filterString = "RowFilter (=, 'binaryprefix:other')"
+        scanner.options = {
+          t_scan: t_scan
+        }
+        scanner.open
+        results = scanner.fetch_rows
+        results.size.must_equal 5
+        scanner.close
+      end # BinaryPrefixComparator binaryprefix
+      
+      it 'RegexStringComparator regexstring' do
+        t_scan = Apache::Hadoop::Hbase::Thrift::TScan.new
+
+        scanner = ThriftHbase::Scanner.new(@table, [], scanner_to_invoke: :with_scan)
+        t_scan.filterString = "RowFilter (=, 'regexstring:sp*')"
+        # with regexstring:*e* => exception
+        scanner.options = {
+          t_scan: t_scan
+        }
+        scanner.open
+        results = scanner.fetch_rows
+        results.size.must_equal 5
+        scanner.close
+      end # RegexStringComparator regexstring
+      
+      it 'SubStringComparator substring' do
+        t_scan = Apache::Hadoop::Hbase::Thrift::TScan.new
+
+        scanner = ThriftHbase::Scanner.new(@table, [], scanner_to_invoke: :with_scan)
+        t_scan.filterString = "RowFilter (=, 'substring:sp')"
+        scanner.options = {
+          t_scan: t_scan
+        }
+        scanner.open
+        results = scanner.fetch_rows
+        results.size.must_equal 5
+        scanner.close
+
+        t_scan = Apache::Hadoop::Hbase::Thrift::TScan.new
+
+        scanner = ThriftHbase::Scanner.new(@table, [], scanner_to_invoke: :with_scan)
+        t_scan.filterString = "RowFilter (=, 'substring:e')"
+        scanner.options = {
+          t_scan: t_scan
+        }
+        scanner.open
+        results = scanner.fetch_rows
+        results.size.must_equal 10
+        scanner.close
+      end # SubStringComparator substring
+      
+    end # '#RowFilter'
   end # 'scannerOpen'
 end # ThriftHbase::Scanner
